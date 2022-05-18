fcee81c3ebe4455da80c14bb6b7b8b570d0b4e4f
amstell27 <sergei_i27@mail.ru>
2022-05-18 13:25:09 +0700

    Add headers for getProductBusketTest

--- src/test/java/tests/api/CitilinkAPITests.java
+++ src/test/java/tests/api/CitilinkAPITests.java
@@ -53,7 +53,7 @@ public class CitilinkAPITests {
                 .log().uri()
                 .log().body()
                 .when()
-                .get("https://www.citilink.ru/basket/add/product/" + parent_id + "/?amount=" + amount + "&parent_id=" + parent_id + "&_=" + token)
+                .get("https://www.citilink.ru/basket/add/product/" + parent_id + "/?amount=" + amount + "&parent_id=" + parent_id + "&_=" + token + " HTTP 1.1")
                 .then()
                 .log().status()
                 .log().body()
