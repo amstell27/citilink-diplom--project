5f27648a3e38269a4de85ed15629d66d34b28c6b
amstell27 <sergei_i27@mail.ru>
2022-05-18 12:54:27 +0700

    Add headers for getProductBusketTest

--- src/test/java/tests/api/CitilinkAPITests.java
+++ src/test/java/tests/api/CitilinkAPITests.java
@@ -46,9 +46,11 @@ public class CitilinkAPITests {
                 token = "1652783436497";
 
         given()
+                .header("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 OPR/86.0.4363.59")
                 .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 OPR/86.0.4363.59")
                 .header("x-requested-with", "XMLHttpRequest")
                 .header("Accept", "*/*")
+                .header("sec-ch-ua-platform", "Windows")
                 .log().headers()
                 .log().uri()
                 .log().body()
