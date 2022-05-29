161b9489a5fc7b3340c65337666507d93ea03eea
amstell27 <sergei_i27@mail.ru>
2022-05-29 23:26:30 +0700

    change annotations JSON_property

--- src/test/java/models/StorageModel.java
+++ src/test/java/models/StorageModel.java
@@ -2,14 +2,12 @@ package models;
 
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class StorageModel {
 
-    @JsonProperty("storage")
     private CartModel storage;
 
 }
--- src/test/java/models/service/SubItemsModel.java
+++ src/test/java/models/service/SubItemsModel.java
@@ -1,13 +1,11 @@
 package models.service;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class SubItemsModel {
 
-    @JsonProperty("subItems")
     private ServiceName subItems;
 }
